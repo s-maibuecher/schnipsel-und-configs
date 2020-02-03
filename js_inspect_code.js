d2bcb5363a781eafb43835c7fee6dacf5606ac48
Stefan Maibücher <s.maibuecher@gmail.com>
2020-02-03 14:11:30 +0100

    Ergänzt

--- js_inspect_code.js
+++ js_inspect_code.js
@@ -6,3 +6,6 @@ let s = execSync('cat challenge/tests/polynomialDivision.test.js');
 console.log(s.toString())
 console.log('****nach exec');
 
+// es geht auch:
+let test_content  = fs.readFileSync('./tvchallenge/tests/tvchannel.test.js');
+console.log(test_content.toString())
